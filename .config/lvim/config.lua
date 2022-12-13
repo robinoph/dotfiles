1e5b7b7e926fb0b4a7403a3e429932b52b2c1024
Robin Ophalvens <robin@ophalvens.eu>
2022-12-13 19:27:04 +0100

    Update lunar vim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,24 +1,9 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
--- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedark"
 lvim.wrap = true
-
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
-
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
@@ -29,17 +14,17 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "c",
   "json",
   "lua",
   "python",
+  "typescript",
   "tsx",
   "css",
   "rust",
   "yaml",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell", "java" }
+lvim.builtin.treesitter.ignore_install = { "java", "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 lvim.plugins = {
@@ -48,12 +33,14 @@ lvim.plugins = {
   { "martinda/Jenkinsfile-vim-syntax" },
   { "thecodesmith/vim-groovy" },
   { "tpope/vim-abolish" },
-  { "lunarvim/colorschemes" },
+  { "catppuccin/nvim" },
+  { "joshdick/onedark.vim" },
+  { "sheerun/vim-polyglot" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.groovy", "Jenkinsfile" },
+  pattern = { "*.groovy", "Jenkinsfile", "Jenkins-release" },
   -- enable wrap mode for json files only
   command = "set tabstop=4 shiftwidth=4",
 })
